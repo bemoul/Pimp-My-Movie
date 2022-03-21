d57710bad78044eab5d4542d25d206e1ce346fe3
curlyroots <selimcodeur@gmail.com>
2022-03-21 22:12:04 +0100

    add migration references categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_203553) do
+ActiveRecord::Schema.define(version: 2022_03_21_210623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_203553) do
     t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
   end
 
   create_table "comments", force: :cascade do |t|
