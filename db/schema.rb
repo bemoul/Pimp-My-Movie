9287dda1ece4ed0bd98d15defa0a00d9ed2c74ec
curlyroots <selimcodeur@gmail.com>
2022-03-21 22:02:48 +0100

    add migration references comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_181157) do
+ActiveRecord::Schema.define(version: 2022_03_21_203553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_181157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_181157) do
   end
 
   create_table "ratings", force: :cascade do |t|
+    t.integer "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
