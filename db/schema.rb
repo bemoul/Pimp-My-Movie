32bb2068780a59fffb3c42ffd34e3a89298425c8
curlyroots <selimcodeur@gmail.com>
2022-03-21 10:29:06 +0100

    table actor  ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_092620) do
+ActiveRecord::Schema.define(version: 2022_03_21_092838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "full_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "description"
