250fdd82d7f9b4d548b1a328ff9e2b98e4b7e437
curlyroots <selimcodeur@gmail.com>
2022-03-21 11:29:32 +0100

    add profiles_controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,23 @@
+class ProfilesController < ApplicationController
+
+  before_action :set_user, only: [:update, :edit, :show]
+
+  def show
+    @user = current_user
+    @user_email = current_user.email
+  end
+
+  def edit
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+end
