908b5c4420d51419232a1b60239f44f22f3980e9
curlyroots <selimcodeur@gmail.com>
2022-03-21 11:29:00 +0100

    add route for profile user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
   root 'home#index'
+  resource 'profiles', only: [:show, :edit], :path => "my-account"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
