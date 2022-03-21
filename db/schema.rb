25b6510917e3cb761a99a67f6ba867408e1e12ce
curlyroots <selimcodeur@gmail.com>
2022-03-21 10:30:37 +0100

    table category ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_092928) do
+ActiveRecord::Schema.define(version: 2022_03_21_093016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "actors", force: :cascade do |t|
     t.string "full_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
