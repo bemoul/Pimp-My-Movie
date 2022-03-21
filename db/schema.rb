bf8dd3c768d6ee1d2c47f469849eb5a247237c2b
curlyroots <selimcodeur@gmail.com>
2022-03-21 10:29:55 +0100

    table music ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_092838) do
+ActiveRecord::Schema.define(version: 2022_03_21_092928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_03_21_092838) do
   create_table "comments", force: :cascade do |t|
     t.text "description"
     t.date "create_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string "title"
+    t.string "band"
+    t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
