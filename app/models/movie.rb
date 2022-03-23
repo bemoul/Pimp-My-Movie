0df269addd15024238fb53be6864e3c1e33de01d
Sarah Coppin <sacoppin@hotmail.com>
2022-03-23 11:52:01 +0100

    app/models/movie.rb delete #has_many :ratings, through: :users)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,8 +5,4 @@ class Movie < ApplicationRecord
   has_many :ratings
   has_many :actors
   has_many :musics
-<<<<<<< HEAD
-=======
-  #has_many :ratings, through: :users
->>>>>>> 73a9240 (add Gemfile.lock)
 end
