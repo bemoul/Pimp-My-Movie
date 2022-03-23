ef14d7de1d91da5583b8a1772d551e9f8bd0fb90
Wally-Lazaar <wally.lazaar@gmail.com>
2022-03-23 09:57:14 +0100

    landing_page

--- db/migrate/20220321094943_add_references_user.rb
+++ db/migrate/20220321094943_add_references_user.rb
@@ -2,6 +2,6 @@ class AddReferencesUser < ActiveRecord::Migration[5.2]
   def change
     add_column :movies, :user_id, :integer
 		add_column :comments, :user_id, :integer
-		add_column :ratings, :user_id, :integer
+	
   end
 end
