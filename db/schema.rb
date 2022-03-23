a9e8f4ce8ceff47acfbcedd93bc94373aed553e1
Wally-Lazaar <wally.lazaar@gmail.com>
2022-03-23 10:03:13 +0100

    landing_page_02

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_105841) do
+ActiveRecord::Schema.define(version: 2022_03_21_094943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,8 +57,6 @@ ActiveRecord::Schema.define(version: 2022_03_21_105841) do
   create_table "ratings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
