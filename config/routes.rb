a93daa1c00a33c90dbd9e6d19df60ef85d935bb2
curlyroots <selimcodeur@gmail.com>
2022-03-23 12:35:51 +0100

    configure acces only current_user to create movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :movies
   root 'home#index'
   resource 'profiles', only: [:show, :edit], :path => "my-account"
-  resource 'movies'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
