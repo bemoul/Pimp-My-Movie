ed36d6c2641fb1b59d5ced9fdb9bc94584dea5c5
Sarah Coppin <sacoppin@hotmail.com>
2022-03-25 19:38:32 +0100

    validation for image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,20 @@
 class Movie < ApplicationRecord
   
   has_one_attached :movie_picture
+  validate :movie_picture
 
+  # def movie_picture
+  #   if movie_picture.attached?
+  #     if movie_picture.blob.byte_size > 1000000
+  #       movie_picture.purge
+  #       errors[:base] << 'Too big'
+  #     elsif !movie_picture.blob.content_type.starts_with?('image/')
+  #       movie_picture.purge
+  #       errors[:base] << 'Wrong format'
+  #     end
+  #   end
+  # end
+  # validates :movie_picture, attached: true, content_type: [:gif, :png, :jpg, :jpeg]
   validates :title, :synopsis, :director, :release_date, presence: true
   validates :release_date, numericality: { greater_than: 1888, less_than:  Date.current.year+1 }
   validates :title, uniqueness: true
