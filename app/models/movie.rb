4b20738b524766035e2e4f631a56821655160cbe
curlyroots <selimcodeur@gmail.com>
2022-03-26 10:21:43 +0100

    add validation active_storage

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,10 @@
 class Movie < ApplicationRecord
   
   has_one_attached :movie_picture
-
   validates :title, :synopsis, :director, :release_date, presence: true
   validates :release_date, numericality: { greater_than: 1888, less_than:  Date.current.year+1 }
   validates :title, uniqueness: true
-  validates :movie_picture, presence: true
+  validates :movie_picture, attached: true, content_type: [:png, :jpg, :jpeg]
   belongs_to :user
   has_many :comments
   has_many :categories
