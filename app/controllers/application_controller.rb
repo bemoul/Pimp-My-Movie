1acb3f2fb990e6ce4922bf53029c0d6e710af17e
Sarah Coppin <sacoppin@hotmail.com>
2022-03-29 11:11:17 +0200

    try add admin button in navbar if admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :admin_user
+
+  # def admin_user
+  #   return if admin_user # or whatever logic to authorize them
+  #   redirect_to admin_user_path(current_user) # fallback if not authorized
+  # end
+  # def authenticate_admin_user!
+  #   if current_admin_user.present? and current_user.blank?
+  #     sign_in current_admin_user, bypass: true
+  #   end
+
+  #   super
+  # end
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,7 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  self.inheritance_column = :_type_disabled
+  # devise :database_authenticatable, 
+  #        :recoverable, :rememberable, :validatable
 end
