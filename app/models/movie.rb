0c97a33d215b24226750a4e9bb575c32d4cf9871
ilyes sedjal <ilyes.sedjal@gmail.com>
2022-04-01 14:56:23 +0200

    adding delete movie ok

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,24 +1,22 @@
 class Movie < ApplicationRecord
-  
+
   has_one_attached :movie_picture
   validates :title, :synopsis, :director, :release_date, presence: true
   validates :release_date, numericality: { greater_than: 1888, less_than:  Date.current.year+1 }
   validates :title, uniqueness: true
-  
-  
-  belongs_to :user
-  has_many :comments
 
-  has_many :movie_actors
+
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  has_many :movie_actors, dependent: :destroy
   has_many :actors, through: :movie_actors
 
   has_many :movie_categories
   has_many :categories, through: :movie_categories
 
-  has_many :ratings
-
-  has_many :movie_musics
+  has_many :ratings, dependent: :destroy
+  has_many :movie_musics, dependent: :destroy
   has_many :musics, through: :movie_musics
 
 end
-
